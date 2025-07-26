295c32af161e61d034ced693fa7a28b39ae90f92
DrozzzD35 <kapustin3595@yandex.ru>
2025-07-26 20:02:55 +0300

    решена 5я задача

---
+++ src/stage_2/task_5/Task.java
@@ -0,0 +1,30 @@
+package stage_2.task_5;
+
+public class Task {
+    int id;
+    String name;
+
+    public Task(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Task newObj = (Task) obj;
+        return id == newObj.id && name.equals(newObj.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + id;
+        return result;
+    }
+}
